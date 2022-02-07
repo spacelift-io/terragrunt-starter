474be363b72eee720467d2f2d35d88a5a15777e3
Chris Spitzenberger <spitz@Chriss-MacBook-Air.local>
2022-02-07 11:45:12 -0600

    remove stack

--- stacks.tf
+++ stacks.tf
@@ -1,13 +1,13 @@
 data "spacelift_current_stack" "this" {}
 
-resource "spacelift_stack" "managed" {
-  name        = "terragrunt-starter/root/test/us-east-1/s3"
-  description = "Your first stack managed by Terraform"
+// resource "spacelift_stack" "managed" {
+//   name        = "terragrunt-starter/root/test/us-east-1/s3"
+//   description = "Your first stack managed by Terraform"
 
-  repository   = "terragrunt-starter"
-  branch       = "main"
-  project_root = "./root/test/us-east-1/s3"
+//   repository   = "terragrunt-starter"
+//   branch       = "main"
+//   project_root = "./root/test/us-east-1/s3"
 
-  autodeploy = true
-  labels     = ["managed", "terragrunt", "depends-on:${data.spacelift_current_stack.this.id}"]
-}
+//   autodeploy = true
+//   labels     = ["managed", "terragrunt", "depends-on:${data.spacelift_current_stack.this.id}"]
+// }
