677fe0907a2ccef0545affd11b4658f03c659661
Chris Spitzenberger <spitz@Chriss-MacBook-Air.local>
2022-02-07 15:40:52 -0600

    Create role and apply to stack

--- stacks.tf
+++ stacks.tf
@@ -11,3 +11,33 @@ resource "spacelift_stack" "managed" {
   autodeploy = true
   labels     = ["managed", "terragrunt", "depends-on:${data.spacelift_current_stack.this.id}"]
 }
+
+# Role assumed by a Stack
+resource "spacelift_stack_aws_role" "spacelift-stack" {
+  stack_id = "terragrunt-starter/root/test/us-east-1/s3"
+  role_arn = aws_iam_role.spacelift.arn
+}
+
+resource "aws_iam_role" "spacelift" {
+  name = "spitzzz-terragrunt-starter-role"
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/PowerUserAccess"
+  ]
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Action" : "sts:AssumeRole",
+        "Condition" : {
+          "StringLike" : {
+            "sts:ExternalId" : "spitzzz@*"
+          }
+        },
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "324880187172"
+        }
+      }
+    ]
+  })
+}
