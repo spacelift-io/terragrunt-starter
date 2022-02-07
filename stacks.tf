89cbeb128eb0ff99081f4f01dea8566cb2fd8313
Chris Spitzenberger <spitz@Chriss-MacBook-Air.local>
2022-02-07 17:04:10 -0600

    delete role

--- stacks.tf
+++ stacks.tf
@@ -13,32 +13,32 @@ resource "spacelift_stack" "managed" {
   labels       = ["managed", "terragrunt", "depends-on:${data.spacelift_current_stack.this.id}"]
 }
 
-// Configure stack to use the role
-resource "spacelift_aws_role" "credentials" {
-  stack_id = spacelift_stack.managed.id
-  role_arn = aws_iam_role.spacelift.arn
-}
+// // Configure stack to use the role
+// resource "spacelift_aws_role" "credentials" {
+//   stack_id = spacelift_stack.managed.id
+//   role_arn = aws_iam_role.spacelift.arn
+// }
 
-resource "aws_iam_role" "spacelift" {
-  name = "spitzzz-terragrunt-starter-role"
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/PowerUserAccess"
-  ]
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Condition": {
-              "StringLike": {
-                "sts:ExternalId": "spitzzz@*"
-              }
-            },
-            "Effect": "Allow",
-            "Principal": {
-              "AWS": "324880187172"
-            }
-          }
-    ]
-  })
+// resource "aws_iam_role" "spacelift" {
+//   name = "spitzzz-terragrunt-starter-role"
+//   managed_policy_arns = [
+//     "arn:aws:iam::aws:policy/PowerUserAccess"
+//   ]
+//   assume_role_policy = jsonencode({
+//     "Version": "2012-10-17",
+//     "Statement": [
+//         {
+//             "Action": "sts:AssumeRole",
+//             "Condition": {
+//               "StringLike": {
+//                 "sts:ExternalId": "spitzzz@*"
+//               }
+//             },
+//             "Effect": "Allow",
+//             "Principal": {
+//               "AWS": "324880187172"
+//             }
+//           }
+//     ]
+//   })
 }
