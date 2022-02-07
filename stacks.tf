da185ac887c77cf304111b462015063680abbe95
Chris Spitzenberger <43446469+spitzzz@users.noreply.github.com>
2022-02-07 11:29:20 -0600

    Feature/refactor terragrunt (#3)

---
+++ stacks.tf
@@ -0,0 +1,13 @@
+data "spacelift_current_stack" "this" {}
+
+resource "spacelift_stack" "managed" {
+  name        = "terragrunt-starter"
+  description = "Your first stack managed by Terraform"
+
+  repository   = "terragrunt-starter"
+  branch       = "main"
+  project_root = "./root/test/us-east-1/s3"
+
+  autodeploy = true
+  labels     = ["managed", "depends-on:${data.spacelift_current_stack.this.id}"]
+}
