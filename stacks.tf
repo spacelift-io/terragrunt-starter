147dc47104d5a2d5ea807b84eb11326c175bafc0
Chris Spitzenberger <spitz@Chriss-MacBook-Air.local>
2022-02-07 16:42:26 -0600

    Switch to stack id

--- stacks.tf
+++ stacks.tf
@@ -17,7 +17,7 @@ resource "spacelift_aws_role" "credentials" {
   depends_on = [
     spacelift_stack.managed
   ]
-  stack_id                       = "terragrunt-starter/root/test/us-east-1/s3"
+  stack_id                       = "terragrunt-starter-root-test-us-east-1-s3"
   role_arn                       = aws_iam_role.spacelift.arn
   generate_credentials_in_worker = true
 }
