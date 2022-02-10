849bd7e5624343774fa6634284058fafb4d2d8b2
Chris Spitzenberger <chriss@spacelift.io>
2022-02-10 10:44:15 -0600

    Add missing variables and format

--- main.tf
+++ main.tf
@@ -5,22 +5,22 @@ resource "aws_iam_role" "spacelift" {
     "arn:aws:iam::aws:policy/PowerUserAccess"
   ]
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Condition": {
-              "StringLike": {
-                "sts:ExternalId": "${var.spaceliftAccount}@*"
-              }
-            },
-            "Effect": "Allow",
-            "Principal": {
-              // This is needed to allow Spacelift to use this role
-              // for applying resources in your AWS account.
-              "AWS": "324880187172"
-            }
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Condition" : {
+          "StringLike" : {
+            "sts:ExternalId" : "${var.spaceliftAccount}@*"
+          }
+        },
+        "Effect" : "Allow",
+        "Principal" : {
+          // This is needed to allow Spacelift to use this role
+          // for applying resources in your AWS account.
+          "AWS" : "324880187172"
         }
+      }
     ]
   })
 }
@@ -36,30 +36,30 @@ resource "spacelift_stack" "managed" {
 
   manage_state = true
   autodeploy   = each.value.autodeploy
-  labels       = concat([
-    "managed", 
+  labels = concat([
+    "managed",
     "terragrunt"
-  ], 
+    ],
     formatlist("depends-on:%s", each.value.dependsOnPaths)
   )
 }
 
 // Stack Role Attachment
 resource "spacelift_aws_role" "credentials" {
-  depends_on  = [
+  depends_on = [
     spacelift_stack.managed
   ]
-  count       = length(var.stacks)
-  stack_id    = values(spacelift_stack.managed)[count.index].id
-  role_arn    = aws_iam_role.spacelift.arn
+  count    = length(var.stacks)
+  stack_id = values(spacelift_stack.managed)[count.index].id
+  role_arn = aws_iam_role.spacelift.arn
 }
 
 // // Stack Policy Attachment
 resource "spacelift_policy_attachment" "policy-attachment" {
-  depends_on  = [
+  depends_on = [
     spacelift_stack.managed
   ]
-  count       = length(var.stacks)
-  policy_id   = "ignore-commits-outside-the-project-root"
-  stack_id    = values(spacelift_stack.managed)[count.index].id
+  count     = length(var.stacks)
+  policy_id = "ignore-commits-outside-the-project-root"
+  stack_id  = values(spacelift_stack.managed)[count.index].id
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
+variable "spaceliftAccount" {
+  type        = string
+  description = "The name of the Spacelift account you are using."
+}
+
+variable "repositoryName" {
+  type        = string
+  description = "The name of the Git repository for this terragrunt-starter."
+}
+
+variable "repositoryBranch" {
+  type        = string
+  description = "The name of the branch to use for the specified Git repository."
+}
+
 variable "stacks" {
-    type    = map
-    default = {}
+  type    = map(any)
+  default = {}
 }
