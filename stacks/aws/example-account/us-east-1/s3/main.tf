31bc28f1f1afb11aeea214aafa42ccdd3ec5ef23
Chris Spitzenberger <chriss@spacelift.io>
2022-02-10 17:34:23 -0600

    remove specifyin acl explicitly

--- stacks/aws/example-account/us-east-1/s3/main.tf
+++ stacks/aws/example-account/us-east-1/s3/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "this" {
   bucket = "spitzzz-testing-spacelift-bucket"
-  acl    = "private"
+  // acl    = "private"
 
   tags = {
     Name        = "spitzzz-testing-spacelift-bucket",
