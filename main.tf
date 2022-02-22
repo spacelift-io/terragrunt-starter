b6a69fd983378990f9c72dd349a0d89f3e29c124
Chris Spitzenberger <chriss@spacelift.io>
2022-02-22 16:04:29 -0600

    Testing fix for attachmentContextIds

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ module "stack" {
   attachmentPolicyIds  = lookup(var.stacks[each.key], "attachmentPolicyIds", [])
   attachmentContextIds = [
     concat(
-      [tostring(spacelift_context.shared.id)],
+      tolist([tostring(spacelift_context.shared.id)]),
       lookup(var.stacks[each.key], "attachmentContextIds", [])
     )
   ]
